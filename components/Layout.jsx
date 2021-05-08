cc0bc0f71d228c1c0d46e8c909128ea7bd272038
Victor Bejas <victorbejas97@gmail.com>
2021-05-08 00:53:33 -0300

    building Layout app

---
+++ components/Layout.jsx
@@ -0,0 +1,38 @@
+import Footer from './Footer'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import styles from 'styles/Layout.module.css'
+
+export default function Layout ({
+  children,
+  title = 'Info Midudev',
+  description = 'Website sobre Información de Midudev'
+}) {
+  return (
+    <>
+      <Head>
+        <meta charSet='UTF-8' />
+        <title>{title}</title>
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='description' content={description} />
+        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+        <meta property='og:locale' content='es-ES' />
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={description} />
+        <meta property='og:url' content='https://midudev.info' />
+        <meta property='og:site_name' content='https://midudev.info' />
+        <meta name='author' content='Victor Bejas' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:creator' content='@victorbejas' />
+        <meta name='twitter:domain' content='https://twitter.com/victorbejas' />
+        <meta name='twitter:site' content='@victorbejas' />
+      </Head>
+      <Navbar />
+      <main className={styles.main}>
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,17 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Navbar from 'components/Navbar'
+import Layout from 'components/Layout'
+import ListCardVideos from 'components/ListCardVideos'
 import Profile from 'components/Profile'
+import SocialMedia from 'components/SocialMedia'
 
 import getYoutubeVideos from 'services/getYoutubeVideos'
-import ListCardVideos from 'components/ListCardVideos'
-import Footer from 'components/Footer'
-import SocialMedia from 'components/SocialMedia'
 
 export default function Home ({ videos }) {
   return (
-    <>
-      <Head>
-        <title>Info MiduDev</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='description' content='Web site sobre Información Midudev' />
-        <link rel='icon' href='/favicon.ico' />
-        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
-        <meta property='og:locale' content='es-ES' />
-        <meta property='og:title' content='Información Midudev' />
-        <meta property='og:description' content='Web site sobre Información Midudev' />
-        <meta property='og:url' content='https://midudev.info' />
-        <meta property='og:site_name' content='https://midudev.info' />
-        <meta name='author' content='Victor Bejas' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:creator' content='@victorbejas' />
-        <meta name='twitter:domain' content='https://twitter.com/victorbejas' />
-        <meta name='twitter:site' content='@victorbejas' />
-      </Head>
-      <Navbar />
-      <main className={styles.main}>
-        <Profile />
-        <SocialMedia />
-        <ListCardVideos videos={videos} />
-      </main>
-      <Footer />
-    </>
+    <Layout>
+      <Profile />
+      <SocialMedia />
+      <ListCardVideos videos={videos} />
+    </Layout>
   )
 }
 
