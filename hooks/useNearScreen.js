612f556e581f67d848a0dec7022b384ba31a88ec
Victor Bejas <54491743+vab1997@users.noreply.github.com>
2021-06-10 12:46:44 -0300

    delete comment

--- hooks/useNearScreen.js
+++ hooks/useNearScreen.js
@@ -22,29 +22,3 @@ export default function useNearScreen ({ distance = '100px' } = {}) {
 
   return { isNearScreen, fromRef }
 }
-
-// import { useState, useEffect, useRef } from "react"
-
-// export default function useNearScreen({ distance = "100px" } = {}) {
-//   const [isNearScreen, setIsNearScreen] = useState(false)
-//   const fromRef = useRef()
-
-//   useEffect(() => {
-//     const onChange = (entries, observer) => {
-//       const el = entries[0]
-//       if (el.isIntersecting) {
-//         setIsNearScreen(true)
-//         observer.disconnect()
-//       }
-//     }
-
-//     const observer = new IntersectionObserver(onChange, {
-//       rootMargin: distance,
-//     })
-
-//     observer.observe(fromRef.current)
-
-//     return () => observer.disconnect()
-//   })
-//   return { isNearScreen, fromRef }
-// }
